372ddcfa5d4970eebdab3a9c8842d27cc42e313e
Huriel Lopes <huriel-lopes@hotmail.com>
2023-02-07 15:40:22 -0300

    feat(Header): dynamic animation

--- components/home/Header/index.tsx
+++ components/home/Header/index.tsx
@@ -5,36 +5,36 @@ interface HeaderProps {
 }
 
 export const Header = ({ dynamic }: HeaderProps) => {
+  if (dynamic) return <DynamicHeader />;
+
+  return <SimpleHeader />;
+};
+
+const DynamicHeader = () => {
   const { isScrolled } = useScroll();
 
-  if (dynamic) {
-    return (
-      <header className="sticky top-0 z-20 pb-2 flex flex-col items-center bg-nth-gray-900 text-white uppercase">
-        {!isScrolled ? <DefaultHeader /> : <SmallHeader />}
-      </header>
-    );
-  }
-
   return (
-    <header className="sticky top-0 z-20 pb-2 flex flex-col items-center bg-nth-gray-900 text-white uppercase">
-      <SmallHeader />
+    <header className="sticky top-0 z-20 pb-2 flex flex-col items-center bg-nth-gray-900 text-white uppercase transition-all">
+      <h1
+        className={`transition-all ${
+          !isScrolled ? 'font-bold mt-12 nth2xs:text-5xl nthxs:text-6xl' : 'font-bold my-2 text-4xl'
+        }`}
+      >
+        NexTech
+      </h1>
+      <h2
+        className={`font-light nth2xs:text-md nthxs:text-lg ${isScrolled ? 'hidden' : 'visible'}`}
+      >
+        Tecnologia de Ponta
+      </h2>
     </header>
   );
 };
 
-const DefaultHeader = () => {
+const SimpleHeader = () => {
   return (
-    <>
-      <h1 className="font-bold mt-12 nth2xs:text-5xl nthxs:text-6xl">NexTech</h1>
-      <h2 className="font-light nth2xs:text-md nthxs:text-lg">Tecnologia de Ponta</h2>
-    </>
-  );
-};
-
-const SmallHeader = () => {
-  return (
-    <>
+    <header className="sticky top-0 z-20 pb-2 flex flex-col items-center bg-nth-gray-900 text-white uppercase transition-all">
       <h1 className="font-bold my-2 text-4xl">NexTech</h1>
-    </>
+    </header>
   );
 };
