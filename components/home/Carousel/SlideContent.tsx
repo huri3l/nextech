845062b59a1d2f02464f7999c9d864ca5912ff78
Huriel Lopes <huriel-lopes@hotmail.com>
2023-02-09 18:57:14 -0300

    fix(home): image carousel

--- components/home/Carousel/SlideContent.tsx
+++ components/home/Carousel/SlideContent.tsx
@@ -13,7 +13,9 @@ export const SlideContent = ({ name, img, bgColor }: CarouselSlideProps) => {
           className="object-cover overflow-visible top-1/2 left-1/2 transform -translate-x-10 -translate-y-14 nthxs:-translate-x-8 nthxs:-translate-y-20"
           src={img.url}
           alt={img.alt}
+          priority
           fill
+          sizes="(max-width: 200px) 100vw, (max-width: 300px ) 50vw"
         />
       </div>
       <h4 className="text-center font-bold drop-shadow-md uppercase text-4xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
