84bd17db1091ad2a0b3029e55864b4234965fd73
Huriel Lopes <huriel-lopes@hotmail.com>
2023-02-12 00:23:41 -0300

    refactor: remove console logs

--- hooks/useMenu.tsx
+++ hooks/useMenu.tsx
@@ -1,8 +1,5 @@
 export const useMenu = () => {
   const isCurrentPage = (currentPath: string, menuLink: string) => {
-    console.log('currentPath', currentPath);
-    console.log('menuLink', menuLink);
-
     if (menuLink === currentPath) return true;
 
     return false;
